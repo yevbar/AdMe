5848d3e989553a76ac8cc23eb1eba9c65ef9bec7
yevbar <yevbar1729@gmail.com>
2018-10-06 03:16:32 -0700

    Fix handles + Style changes

--- src/App.js
+++ src/App.js
@@ -4,32 +4,65 @@ import './App.css';
 import happiness from './media/happiness.png';
 import kate from './media/kate.jpg';
 import yev from './media/yev.jpg';
+import gregg from './media/gregg.jpg';
+// import new images here
+
+let content = [
+    {
+	"description": "Check out my drink!",
+	"image": yev,
+	"ad": false
+    },
+    {
+	"description": "Open your happiness with Coca Cola",
+	"image": happiness,
+	"ad": true
+    },
+    {
+	"description": "Look at this sexy motherfucker",
+	"image": gregg,
+	"ad": false
+    },
+    {
+	"description": "Drink your Coca Cola with Kate",
+	"image": kate,
+	"ad": true
+    }
+];
+
+// replace sources with new images and also remove "ads"
+let content2 = [
+    {
+	"description": "Check out my drink!",
+	"image": yev,
+	"ad": false
+    },
+    {
+	"description": "Open your happiness with Coca Cola",
+	"image": happiness,
+	"ad": true
+    },
+    {
+	"description": "Look at this sexy motherfucker",
+	"image": gregg,
+	"ad": false
+    },
+    {
+	"description": "Drink your Coca Cola with Kate",
+	"image": kate,
+	"ad": true
+    }
+];
 
 class App extends Component {
+    constructor(props) {
+	super(props);
+	this.state = {content: content}
+    }
+
     render() {
 	const unit = 500;
-	let content = [
-	    {
-		"description": "Check out my drink!",
-		"image": yev,
-		"ad": false
-	    },
-	    {
-		"description": "Open your happiness with Coca Cola",
-		"image": happiness,
-		"ad": true
-	    },
-	    {
-		"description": "Here's another fucking picture",
-		"image": null,
-		"ad": false
-	    },
-	    {
-		"description": "Drink your Coca Cola with Kate",
-		"image": kate,
-		"ad": true
-	    }
-	];
+	
     return (
 	    <div className="App" style={{}}>
 	    <div style={{backgroundColor: "#ffffff", textAlign: "center"}}>
@@ -37,14 +70,19 @@ class App extends Component {
 	    </div>
 	    <div style={{marginTop: "5em"}}>
 	    {
-		content.map((e) => (
-			<div style={{display: "flex", justifyContent: "center"}}>
+		this.state.content.map((e) => (
+			<div style={{display: "flex", justifyContent: "center", border: "1px solid #E1E8ED"}}>
 			<div style={{textAlign: "center", marginBottom: "2em", padding: "1.5em", backgroundColor: "#ffffff"}}>
 			<div style={{display: "flex", flexDirection: "row", alignContent: "center"}}>
-			<div style={{width: "1.5em", height: "1.5em", borderRadius: "2em", overflow: "hidden"}}>
-			<img style={{width: "1.5em", height: "1.5em"}} src="https://pbs.twimg.com/profile_images/716641091311697920/hFFVBhFe_400x400.jpg" />
+			<div style={{width: "2em", height: "2em", borderRadius: "2em", overflow: "hidden"}}>
+			<img style={{width: "2em", height: "2em"}} src="https://pbs.twimg.com/profile_images/716641091311697920/hFFVBhFe_400x400.jpg" />
 			</div>
-			<span style={{marginLeft: "0.5em", marginTop: "0.25em"}}>{e.description}</span>
+			<div style={{marginLeft: "0.5em", textAlign: "left"}}>
+			{
+			    (!e.ad) ? <div><span>Y Combinator</span><span style={{marginLeft: "0.25em", color: "#657786"}}>@ycombinator</span></div> : <div><span>Coca Cola</span><span style={{marginLeft: "0.25em", color: "#657786"}}>@CocaCola</span></div>
+			}
+			<span style={{marginTop: "0.25em"}}>{e.description}</span>
+			</div>
 			</div>
 			<div style={{width: unit, height: unit, marginTop: "2em", marginRight:"auto", marginLeft: "auto"}}>
 			<Tilt className="tilt" options={{max: 25}}>
